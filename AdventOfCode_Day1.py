bc1babe2a21eba507a9c3c7a81ddbac882029e38
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-03-23 07:10:24 +0100

    came up with different solutions in AdventOfCode_Day1.py

--- AdventOfCode_Day1.py
+++ AdventOfCode_Day1.py
@@ -1,3 +1,5 @@
+#3rd solution:
+'''
 list1values = input()
 list1Values1 = list(map(int, list1values.split()))
 list2values = input()
@@ -9,4 +11,36 @@ for i, j in zip(sorted1, sorted2):
     distance.append(abs(j-i))
 totalSum = sum(distance)
 print(totalSum)
+'''
+
+
+
+#2nd solution:
+'''
+left_value = []
+right_value = []
+
+try:
+    while True:
+        line = input().strip()
+        if not line:
+            break
+        left, right = map(int, line.split())
+        left_value.append(left)
+        right_value.append(right)
+except EOFError:
+    pass
+sorted1 = sorted(left_value)
+sorted2 = sorted(right_value)
+distance = []
+for i, j in zip(sorted1, sorted2):
+    distance.append(j-i)
+totalSum = sum(distance)
+print(totalSum)
+'''
+
+#3rd solution that works:
+data = [*map(int, open('input1.txt').read().split())]
+A, B = sorted(data[0::2]), sorted(data[1::2])
+print(sum(map(lambda a, b: abs(a-b), A, B)))
 
