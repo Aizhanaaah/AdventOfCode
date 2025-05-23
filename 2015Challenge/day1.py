cc7f9b0c2c62d867f02f4c1784510be2aa826d41
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-05-24 00:18:14 +0200

    Update day1.py

--- 2015Challenge/day1.py
+++ 2015Challenge/day1.py
@@ -1,3 +1,5 @@
+#part A:
+
 with open("floors.txt") as f:
     floor = 0
     for i in f.read():
@@ -6,3 +8,20 @@ with open("floors.txt") as f:
         else:
             floor -= 1
 print(floor)
+
+
+#part B:
+
+with open("floors.txt") as f:
+    floor = 0
+    position = 0
+    for i in f.read():
+        if i == '(':
+            floor += 1
+            position += 1
+        else:
+            floor -= 1
+            position += 1
+        if floor == -1:
+            break
+print(position)
