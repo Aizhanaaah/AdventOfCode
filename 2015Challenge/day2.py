d9bf30cfd9adbe0cc2dd3f0b749d66d7b7d86ec2
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-05-25 11:38:28 +0200

    solved the part B of Day2

--- 2015Challenge/day2.py
+++ 2015Challenge/day2.py
@@ -1,4 +1,4 @@
-# Part A:
+#    Part A:
 
 with open("giftsdim.txt") as f:
     line1 = []
@@ -8,5 +8,19 @@ with open("giftsdim.txt") as f:
         w = result[1]
         h = result[2]
         result = 2*l*w + 2*w*h + 2*h*l + min([l*w, w*h, h*l])
+        line1.append(result)
+    print(sum(line1))
+
+
+
+#    Part B
+with open("giftsdim.txt") as f:
+    line1 = []
+    for line in f:
+        result = list(map(int, line.strip().split('x')))
+        l = result[0]
+        w = result[1]
+        h = result[2]
+        result = l*w*h + min([2*l + 2*w, 2*w + 2*h, 2*h + 2*l])
         line1.append(result)
     print(sum(line1))
