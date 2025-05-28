49022d7bea1e2f6b04a7e666ce4e65a49995c3fa
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-05-28 21:53:34 +0200

    Solved the part B of day 3

--- 2015Challenge/day3.py
+++ 2015Challenge/day3.py
@@ -17,3 +17,38 @@ with open("locations.txt") as f:
             y -= 1
         myset.add((x, y))
     print(len(myset))
+
+
+#    Part B:
+with open("locations.txt") as f:
+    x = 0
+    y = 0
+    x1 = 0
+    y1 = 0
+    myset = set()
+    myset.add((x, y))
+    myset.add((x1, y1))
+    text = f.read()
+    for i in range(len(text)):
+        char = text[i]
+        if i % 2 == 0:
+            if char == '>':
+                x += 1
+            elif char == '<':
+                x -= 1
+            elif char == '^':
+                y += 1
+            elif char == 'v':
+                y -= 1
+            myset.add((x, y))
+        else:
+            if char == '>':
+                x1 += 1
+            elif char == '<':
+                x1 -= 1
+            elif char == '^':
+                y1 += 1
+            elif char == 'v':
+                y1 -= 1
+            myset.add((x1, y1))
+    print(len(myset))
